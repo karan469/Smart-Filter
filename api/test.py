8865520a5bd226b528f4bd3e5496189550073309
Crekkaran <cs5170411@iitd.ac.in>
2020-07-24 12:38:42 +0530

    ..

--- api/test.py
+++ api/test.py
@@ -11,21 +11,27 @@ import cv2
 #-------------------- DETECTRON2 --------------------#
 print('C 2')
 
-DetectronObj = detectron()
+# DetectronObj = detectron()
 
 # createThumbnail('snowme.jpg')
-new_image = utils.feature_1(DetectronObj, './snowme.png')
-cv2.imwrite('./x_snowme.png', new_image)
+# new_image = utils.feature_1(DetectronObj, './snowme.png')
+# cv2.imwrite('./x_snowme.png', new_image)
 
 print('C 3')
 
 #-------------------- DETECTRON2 - Face detection --------------------#
 print('C 4')
 
-Face = facedetector(modelname='COCO-Detection/retinanet_R_101_FPN_3x.yaml', filename='./model_final.pth')
-im = cv2.imread('./snowme.png', cv2.IMREAD_COLOR)
+# Face = facedetector(modelname='COCO-Detection/retinanet_R_101_FPN_3x.yaml', filename='./model_final.pth')
+# im = cv2.imread('./snowme.png', cv2.IMREAD_COLOR)
 
-pred_boxes = Face.return_pred_boxes(im)
-print(pred_boxes)
+# pred_boxes = Face.return_pred_boxes(im)
+# print(pred_boxes)
 
 print('C 5')
+
+#-------------------- Background addition --------------------#
+DetectronObj = detectron()
+# utils.createThumbnail('me_winter.jpg')
+new_image = utils.feature_2(DetectronObj, './snowme.png', 'leaves,green')
+cv2.imwrite('./x_back_snowme_1.png', new_image)
